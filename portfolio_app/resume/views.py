fda0ba049808888912c96e3690b8ec0968121ee3
michu999 <mgoluch2020@gmail.com>
2025-05-26 21:15:55 +0200

    updated resume.html, added context to the views

--- portfolio_app/resume/views.py
+++ portfolio_app/resume/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from resume.models import Education, Experience, Skill
 # Create your views here.
 def resume(request):
     """
     Render the home page of the resume application.
     """
-    return render(request, 'resume.html')
+    context = {
+        'educations': Education.objects.all(),
+        'experiences': Experience.objects.all(),
+        'skills': Skill.objects.all(),
+    }
+
+    return render(request, 'resume.html', context=context)
