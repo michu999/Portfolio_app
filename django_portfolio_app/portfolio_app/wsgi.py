2cd7e816dcee692bed41c455821216fd3001496f
michu999 <mgoluch2020@gmail.com>
2025-06-07 21:06:46 +0200

    dfsd

--- django_portfolio_app/portfolio_app/wsgi.py
+++ django_portfolio_app/portfolio_app/wsgi.py
@@ -2,9 +2,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 if os.environ.get('RENDER', '') == 'true':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio_app.portfolio_app.settings')
 else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio_app.portfolio_app.settings')
 
 application = get_wsgi_application()
 app = application
