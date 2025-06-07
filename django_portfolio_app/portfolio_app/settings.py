0feed7f9328e20666995c05200079905b4274cbc
michu999 <mgoluch2020@gmail.com>
2025-06-07 20:56:41 +0200

    changed wsgi

--- django_portfolio_app/portfolio_app/settings.py
+++ django_portfolio_app/portfolio_app/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_portfolio_app.accounts',
+    'accounts',
     'forum',
     'resume',
     'projects',
