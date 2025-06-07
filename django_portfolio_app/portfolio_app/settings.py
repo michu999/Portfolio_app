8438bf55a8dfef2b9a84e6ef70bacee57a800646
michu999 <mgoluch2020@gmail.com>
2025-06-07 20:47:51 +0200

    trying something

--- django_portfolio_app/portfolio_app/settings.py
+++ django_portfolio_app/portfolio_app/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'django_portfolio_app.accounts',
     'forum',
     'resume',
     'projects',
