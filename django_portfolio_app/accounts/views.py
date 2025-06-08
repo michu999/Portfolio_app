b5a59a66f92d9c420d1c0ad0737305a7d00fb720
michu999 <mgoluch2020@gmail.com>
2025-06-08 16:12:34 +0200

    fixed a typo

--- django_portfolio_app/accounts/views.py
+++ django_portfolio_app/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from accounts.models import CustomUser
+from .models import CustomUser
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileEditForm
 
